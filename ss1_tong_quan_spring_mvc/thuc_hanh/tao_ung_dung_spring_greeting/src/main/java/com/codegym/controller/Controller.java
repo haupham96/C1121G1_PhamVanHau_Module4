9d3fbaece5a56f774887c75f18877c6ad1dcc0c3
haupham96 <hau.pv.0596@gmail.com>
2022-03-29 10:33:06 +0700

    ss1 Tong Quan Spring MVC

---
+++ ss1_tong_quan_spring_mvc/thuc_hanh/tao_ung_dung_spring_greeting/src/main/java/com/codegym/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.codegym.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "/index";
+    }
+}
