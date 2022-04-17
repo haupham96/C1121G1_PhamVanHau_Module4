019544296b63f55ded954be05606af1a0609539f
haupham96 <hau.pv.0596@gmail.com>
2022-04-17 16:10:48 +0700

    TH i18n

--- ss13_i18n/thuc_hanh/login/src/main/java/login/configuration/AppConfig.java
+++ ss13_i18n/thuc_hanh/login/src/main/java/login/configuration/AppConfig.java
@@ -7,13 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource() {
@@ -23,6 +24,7 @@ public class AppConfig {
         return messageSource;
     }
 
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
         interceptor.setParamName("lang");
